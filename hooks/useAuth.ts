c1b1e324185063ff64f49990646228a79206ca7f
Kris <kriistjanm@gmail.com>
2021-11-15 11:14:14 +0700

    Move state into zustand and only call useAuth once -- Add check for cancelled/paused

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,21 @@
-import { User } from '@supabase/gotrue-js'
+/* eslint-disable react-hooks/exhaustive-deps */
 import { supabase } from 'functions/supabase'
 import { useEffect, useState } from 'react'
+import { authState } from 'state/authState'
 
 export function useAuth() {
-	const [user, setUser] = useState<User>()
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
-	const [isPro, setIsPro] = useState(false)
-	const [checked, setChecked] = useState(false)
+	const user = authState((state) => state.user)
+	const setUser = authState((state) => state.setUser)
+	const isLoggedIn = authState((state) => state.isLoggedIn)
+	const setIsLoggedIn = authState((state) => state.setIsLoggedIn)
+	const isPro = authState((state) => state.isPro)
+	const setIsPro = authState((state) => state.setIsPro)
+	const checked = authState((state) => state.checked)
+	const setChecked = authState((state) => state.setChecked)
 
 	useEffect(() => {
+		console.log('hello from useeffect')
+
 		const { data: authListener } = supabase.auth.onAuthStateChange(
 			(event, session) => {
 				if (event === 'SIGNED_IN') {
@@ -32,15 +39,39 @@ export function useAuth() {
 		return () => authListener?.unsubscribe()
 	}, [])
 
-	// TODO add checks if cancelled to compare to last payment date?
 	async function checkUser() {
-		const userCheck = supabase.auth.user()
+		let userCheck = supabase.auth.user()
 
+		// if logged in
 		if (userCheck) {
 			setUser(userCheck)
 			setIsLoggedIn(true)
-			if (userCheck.user_metadata.status === 'active') {
-				setIsPro(true)
+
+			// if pro
+			let { data } = await supabase.from('userdata').select()
+			if (data) {
+				console.log(data)
+				console.log(data[0].status)
+
+				let status = data[0].status
+
+				if (['trialing', 'active', 'past_due'].includes(status)) {
+					setIsPro(true)
+				}
+
+				if (status === 'deleted' || status === 'paused') {
+					let stopDate =
+						data[0].cancelled_date ?? data[0].paused_date ?? null
+
+					if (stopDate) {
+						let today = new Date()
+						let stop = new Date(stopDate)
+
+						if (today < stop) {
+							setIsPro(true)
+						}
+					}
+				}
 			}
 		}
 		setChecked(true)
