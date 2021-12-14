894fa3821bdf0a495956ed2ab740a279e564ba42
Kris Gunnars <kriistjanm@gmail.com>
2021-12-14 08:43:35 +0000

    Adjust ads loaded

--- components/Ads/Snigel/LoadAds.tsx
+++ components/Ads/Snigel/LoadAds.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
-import Script from 'next/script';
-import { navState } from 'state/navState';
-import { PathType } from 'types/Path';
+import { useEffect, useState } from 'react'
+import Script from 'next/script'
+import { navState } from 'state/navState'
+import { PathType } from 'types/Path'
 
 declare global {
 	// eslint-disable-next-line no-unused-vars
 	interface Window {
-		adngin: any;
+		adngin: any
 	}
 }
 
 function getPageAds(path: PathType) {
 	// Front page
 	if (!path.one) {
-		return ['top_leaderboard', 'in-content_1_mobile'];
+		return ['in-content_1_mobile']
 	}
 
 	// IPO pages
@@ -23,28 +23,28 @@ function getPageAds(path: PathType) {
 				'top_leaderboard',
 				'sidebar_1',
 				'sidebar_2',
-				'in-content_1_mobile',
-			];
+				'in-content_1_mobile'
+			]
 		}
 		if (path.two === 'statistics') {
-			return ['top_leaderboard', 'sidebar_1'];
+			return ['top_leaderboard', 'sidebar_1']
 		}
 		if (path.two === 'screener') {
-			return ['top_leaderboard'];
+			return ['top_leaderboard']
 		}
-		return ['top_leaderboard', 'sidebar_1', 'sidebar_2'];
+		return ['top_leaderboard', 'sidebar_1', 'sidebar_2']
 	}
 
 	// News and Actions pages
 	if (path.one === 'news' || path.one === 'actions') {
-		return ['top_leaderboard', 'sidebar_1', 'sidebar_2'];
+		return ['top_leaderboard', 'sidebar_1', 'sidebar_2']
 	}
 
 	// Stocks and ETF pages
 	if (path.one === 'stocks' || path.one === 'etf') {
 		// Index pages
 		if (!path.two) {
-			return ['top_leaderboard', 'sidebar_1', 'sidebar_2'];
+			return ['top_leaderboard', 'sidebar_1', 'sidebar_2']
 		}
 		// Overview, statistics, company, dividend pages
 		if (
@@ -53,41 +53,41 @@ function getPageAds(path: PathType) {
 			path.three === 'company' ||
 			path.three === 'dividend'
 		) {
-			return ['top_leaderboard', 'sidebar_1', 'in-content_1_mobile'];
+			return ['top_leaderboard', 'sidebar_1', 'in-content_1_mobile']
 		}
 		// Holdings pages
 		if (path.three === 'holdings') {
-			return ['top_leaderboard', 'sidebar_1', 'sidebar_2'];
+			return ['top_leaderboard', 'sidebar_1', 'sidebar_2']
 		}
 	}
 
 	// Screener page
 	if (path.one === 'stock-screener') {
-		return ['top_leaderboard'];
+		return ['top_leaderboard']
 	}
 
 	// Trending page
 	if (path.one === 'trending') {
-		return ['top_leaderboard', 'sidebar_1'];
+		return ['top_leaderboard', 'sidebar_1']
 	}
 
 	// Mostly article pages
 	if (path.one !== 'stocks' && path.one !== 'etf') {
-		return ['top_leaderboard', 'sidebar_1', 'sidebar_2'];
+		return ['top_leaderboard', 'sidebar_1', 'sidebar_2']
 	}
 
 	// Default
-	return ['top_leaderboard'];
+	return ['top_leaderboard']
 }
 
 export function LoadAds() {
-	const [ads, setAds] = useState<string[]>([]);
-	const path = navState((state) => state.path);
+	const [ads, setAds] = useState<string[]>([])
+	const path = navState((state) => state.path)
 
 	useEffect(() => {
-		let adsArray = getPageAds(path);
+		let adsArray = getPageAds(path)
 
-		setAds(adsArray);
+		setAds(adsArray)
 		if (
 			adsArray.length > 0 &&
 			window.adngin &&
@@ -98,29 +98,29 @@ export function LoadAds() {
 					if (window.innerWidth >= 768) {
 						adsArray = adsArray.filter(
 							(ad) => ad !== 'in-content_1_mobile'
-						);
+						)
 					} else if (window.innerWidth < 768) {
 						adsArray = adsArray.filter(
 							(ad) =>
 								ad === 'in-content_1_mobile' || ad === 'top_leaderboard'
-						);
+						)
 					}
 				}
 
-				window.adngin.cmd.startAuction(adsArray);
-			});
+				window.adngin.cmd.startAuction(adsArray)
+			})
 		}
-	}, [path]);
+	}, [path])
 
 	if (
 		typeof window === 'undefined' ||
 		process.env.NODE_ENV === 'development'
 	) {
-		return null;
+		return null
 	}
 
 	if (ads.length === 0) {
-		return null;
+		return null
 	}
 
 	return (
@@ -131,7 +131,7 @@ export function LoadAds() {
 				dangerouslySetInnerHTML={{
 					__html: `window.snigelPubConf = { "adengine": { "activeAdUnits": ${JSON.stringify(
 						ads
-					)} } }`,
+					)} } }`
 				}}
 			/>
 			<Script
@@ -140,5 +140,5 @@ export function LoadAds() {
 				src="https://cdn.snigelweb.com/adengine/stockanalysis.com/loader.js"
 			/>
 		</>
-	);
+	)
 }
