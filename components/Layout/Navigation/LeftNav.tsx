1d4f4d3c0a9ad71fab86dee807e1ae2abb908c87
Kris Gunnars <kriistjanm@gmail.com>
2021-12-16 14:56:01 +0000

    Show mobile menu with React transition

--- components/Layout/Navigation/LeftNav.tsx
+++ components/Layout/Navigation/LeftNav.tsx
@@ -1,3 +1,4 @@
+import { Transition } from '@headlessui/react'
 import { navMenuState } from 'state/navMenuState'
 import { MainNav } from './MainNav'
 
@@ -5,10 +6,27 @@ export function LeftNav() {
 	const visible = navMenuState((state) => state.visible)
 
 	return (
-		<aside className={visible ? 'leftcol showmenu' : 'leftcol'}>
-			<div className="leftnav">
-				<MainNav />
-			</div>
-		</aside>
+		<>
+			<Transition
+				show={visible}
+				enter="transition ease-in-out duration-300 transform"
+				enterFrom="-translate-x-full"
+				enterTo="translate-x-0"
+				leave="transition ease-in-out duration-300 transform"
+				leaveFrom="translate-x-0"
+				leaveTo="-translate-x-full"
+			>
+				<aside className="mobilemenu">
+					<div className="leftnav">
+						<MainNav />
+					</div>
+				</aside>
+			</Transition>
+			<aside className="leftcol">
+				<div className="leftnav">
+					<MainNav />
+				</div>
+			</aside>
+		</>
 	)
 }
