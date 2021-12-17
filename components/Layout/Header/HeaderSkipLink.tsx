054caef5494f97659251cbf681c30c19415cdaee
Kris Gunnars <kriistjanm@gmail.com>
2021-12-17 09:01:51 +0000

    Flyout Menu: Added skip to main content link to new header

---
+++ components/Layout/Header/HeaderSkipLink.tsx
@@ -0,0 +1,7 @@
+export function HeaderSkipLink() {
+	return (
+		<a href="#main" className="sr-only focus:not-sr-only">
+			Skip to main content
+		</a>
+	)
+}
--- components/Layout/Header/_Header.tsx
+++ components/Layout/Header/_Header.tsx
@@ -2,11 +2,15 @@ import { HeaderLoginNew } from './HeaderLoginNew'
 import { HeaderLogoFull } from './HeaderLogoFull'
 import { HeaderMenuToggle } from './HeaderMenuToggle'
 import { HeaderSearch } from './HeaderSearch'
+import { HeaderSkipLink } from './HeaderSkipLink'
 
 export function Header() {
 	return (
 		<header className="hd-wrap">
 			<div className="hd-inner">
+				{/* Skip Link */}
+				<HeaderSkipLink />
+
 				{/* Menu Button */}
 				<HeaderMenuToggle />
 
