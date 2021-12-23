7015c55ae359a1f44a1a7bcfeb9bd99b12ef58dc
kristjanmar <kriistjanm@gmail.com>
2021-12-23 14:43:05 +0000

    Adjust spacing around ads

--- components/Ads/Snigel/HeaderAd.tsx
+++ components/Ads/Snigel/HeaderAd.tsx
@@ -10,9 +10,9 @@ export function HeaderAd() {
 		if (!isLoggedIn) {
 			return (
 				<aside id="ad-banner">
-					<div className="pt-1 -mb-2 md:-mb-3 mx-auto text-center header-ad">
+					<div className="pt-1 -mb-1.5 md:-mb-2 mx-auto text-center header-ad">
 						<div
-							className="min-h-[71px] md:min-h-[111px] md:max-w-[728px] lg:max-w-[970px] mx-auto text-center center-children overflow-x-auto"
+							className="min-h-[63px] md:min-h-[103px] md:max-w-[728px] lg:max-w-[970px] mx-auto text-center center-children overflow-x-auto"
 							id="adngin-top_leaderboard-0"
 						></div>
 					</div>
