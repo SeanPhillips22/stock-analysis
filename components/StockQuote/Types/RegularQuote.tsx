47f3a03d223d8ca75ba65163b5f67706c61fd00f
Kris Gunnars <kriistjanm@gmail.com>
2021-12-30 16:12:52 +0000

    Added basic pulse animation to stock price

--- components/StockQuote/Types/RegularQuote.tsx
+++ components/StockQuote/Types/RegularQuote.tsx
@@ -1,4 +1,6 @@
+import { usePrevious } from 'hooks/usePrevious'
 import { useQuote } from 'hooks/useQuote'
+import { useEffect, useState } from 'react'
 import { Info } from 'types/Info'
 import { changeColor } from '../quote.functions'
 
@@ -6,11 +8,35 @@ export function RegularQuote({ info }: { info: Info }) {
 	const quote = useQuote(info)
 	const color = changeColor(Number(quote.c))
 	const market = quote.ms == 'open' ? 'Market open' : 'Market closed'
+	const [animation, setAnimation] = useState('')
+
+	const prev = usePrevious(quote.p)
+
+	useEffect(() => {
+		if (quote.p > prev) {
+			console.log('up')
+			setAnimation('pulseup 0.8s linear 0s')
+			setTimeout(() => {
+				setAnimation('')
+			}, 800)
+		} else if (quote.p < prev) {
+			console.log('down')
+			setAnimation('pulsedown 0.8s linear 0s')
+			setTimeout(() => {
+				setAnimation('')
+			}, 800)
+		} else {
+			console.log('same')
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [quote])
 
 	return (
 		<div>
-			<span className="text-4xl font-bold">{quote.pd}</span>{' '}
-			<span className="text-2xl font-semibold">
+			<span className="text-4xl font-bold" style={{ animation }}>
+				{quote.pd}
+			</span>{' '}
+			<span className="text-2xl font-semibold" style={{ animation }}>
 				<span className={color}>{`${quote.c || '0.00'} (${
 					quote.cp || '0.00'
 				}%)`}</span>
