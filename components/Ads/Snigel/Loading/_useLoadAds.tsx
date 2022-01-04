abb4fa55ff9ebe9107baf5f338a114a5fde82418
Kris Gunnars <kriistjanm@gmail.com>
2022-01-04 07:11:32 +0000

    Adjust ad loading

--- components/Ads/Snigel/Loading/_useLoadAds.tsx
+++ components/Ads/Snigel/Loading/_useLoadAds.tsx
@@ -1,18 +1,17 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { navState } from 'state/navState'
-import { getAdsForPage } from './getAdsForPage'
 import { startAdsAuction } from './startAdsAuction'
 
 export function useLoadAds() {
 	const path = navState((state) => state.path)
 	const router = useRouter()
 
-	function loadAds() {
-		startAdsAuction(getAdsForPage(path))
-	}
-
 	useEffect(() => {
+		function loadAds() {
+			startAdsAuction(path)
+		}
+
 		// Load ads when the route has changed
 		router.events.on('routeChangeComplete', loadAds)
 
--- components/Ads/Snigel/Loading/startAdsAuction.ts
+++ components/Ads/Snigel/Loading/startAdsAuction.ts
@@ -1,3 +1,6 @@
+import { getAdsForPage } from './getAdsForPage'
+import { PathType } from 'types/Path'
+
 declare global {
 	// eslint-disable-next-line no-unused-vars
 	interface Window {
@@ -9,27 +12,27 @@ declare global {
  * Start the header bidding auction for the Snigel ads
  * @param ads an array of ad names to show on the page
  */
-export function startAdsAuction(ads: string[]) {
-	let adsArray = ads
+export function startAdsAuction(path: PathType) {
+	let ads = getAdsForPage(path)
 
 	// Check if a) there are ads to display and b) the loader is ready
 	if (ads.length > 0 && window.adngin && window.adngin.adnginLoaderReady) {
 		// Remove unnecessary ads on desktop/mobile
 		if (window.innerWidth) {
 			if (window.innerWidth >= 768) {
-				adsArray = ads.filter((ad) => ad !== 'in-content_1_mobile')
+				ads = ads.filter((ad) => ad !== 'in-content_1_mobile')
 			} else {
-				adsArray = ads.filter(
+				ads = ads.filter(
 					(ad) => ad === 'in-content_1_mobile' || ad === 'top_leaderboard'
 				)
 			}
 		}
 
-		console.log(adsArray)
+		console.log({ ads, path })
 
 		// Start the ads auction
 		window.adngin.queue.push(function () {
-			window.adngin.cmd.startAuction(adsArray)
+			window.adngin.cmd.startAuction(ads)
 		})
 	}
 }
