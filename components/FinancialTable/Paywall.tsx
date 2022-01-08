20f81b41832575bf3b79c116e6a040fbf4a69858
Kris Gunnars <kriistjanm@gmail.com>
2022-01-08 18:23:15 +0000

    Financials paywall: Adjust some edge cases

--- components/FinancialTable/Paywall.tsx
+++ components/FinancialTable/Paywall.tsx
@@ -1,9 +1,10 @@
 import { LockClosedIcon } from '@heroicons/react/solid'
 import { useRouter } from 'next/router'
+import { Range } from 'types/Financials'
 import { formatYear } from './FinancialTable.functions'
 
 type PropsHeaderCell = {
-	range: string
+	range: Range
 	diff: number
 	last: string
 }
@@ -13,6 +14,10 @@ export function PaywallHeaderCell({ range, diff, last }: PropsHeaderCell) {
 		let yearStart = Number(formatYear(last)) - 1
 		let yearEnd = yearStart - diff + 1
 
+		if (yearStart === yearEnd) {
+			return <th className="lockhead">{`${yearStart}`}</th>
+		}
+
 		return <th className="lockhead">{`${yearStart} - ${yearEnd}`}</th>
 	}
 
@@ -20,7 +25,7 @@ export function PaywallHeaderCell({ range, diff, last }: PropsHeaderCell) {
 }
 
 type PropsBodyCell = {
-	range: string
+	range: Range
 	showcount: number
 	fullcount: number
 }
@@ -32,6 +37,7 @@ export function PaywallBodyCell({
 }: PropsBodyCell) {
 	const type = range === 'annual' ? 'years' : 'quarters'
 	const countShown = range === 'annual' ? showcount - 1 : showcount
+	const countFull = range === 'annual' ? fullcount - 1 : fullcount
 	const router = useRouter()
 
 	function goToTrialPage() {
@@ -41,12 +47,12 @@ export function PaywallBodyCell({
 	return (
 		<td className="lockcell">
 			<div
-				title={`Showing ${countShown} of ${fullcount} ${type}`}
+				title={`Showing ${countShown} of ${countFull} ${type}`}
 				onClick={goToTrialPage}
 				id="tag-upgr-financials-table-cell"
 			>
 				<span>Upgrade</span>
-				<LockClosedIcon className="ml-1 w-3.5 h-3.5 text-gray-500" />
+				<LockClosedIcon className="ml-1 w-3.5 h-3.5" />
 			</div>
 		</td>
 	)
