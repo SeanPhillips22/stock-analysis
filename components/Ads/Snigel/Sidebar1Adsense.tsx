0f4a2d10059fc8187b7ea87fcff840fef2e536be
Kris Gunnars <kriistjanm@gmail.com>
2022-01-12 10:33:26 +0000

    AdSense test

--- components/Ads/Snigel/Sidebar1Adsense.tsx
+++ components/Ads/Snigel/Sidebar1Adsense.tsx
@@ -1,6 +1,6 @@
 import { useAuthState } from 'hooks/useAuthState'
 import Script from 'next/script'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 // import { navState } from 'state/navState'
 // import { noAds } from 'components/Ads/noAds'
 
@@ -11,13 +11,13 @@ declare global {
 }
 
 export function Sidebar1Adsense() {
+	const [count, setCount] = useState(0)
 	// const path = navState((state) => state.path)
 
 	// Fill the slot with an AdSense ad
 	function loadAdsense() {
 		try {
-			let adsbygoogle = window.adsbygoogle
-			console.log({ adsbygoogle })
+			const adsbygoogle = window.adsbygoogle || []
 			adsbygoogle.push({})
 		} catch (e) {
 			console.error(e)
@@ -27,13 +27,20 @@ export function Sidebar1Adsense() {
 	// Check if AdSense script is loaded every 300ms
 	useEffect(() => {
 		let interval = setInterval(() => {
+			setCount((count) => count + 1)
+
+			console.log('count', count)
 			if (window.adsbygoogle) {
+				console.log('load')
 				loadAdsense()
 				clearInterval(interval)
 			}
+
+			if (count === 6) clearInterval(interval)
 		}, 300)
 
 		return () => clearInterval(interval)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	const { checked, isPro } = useAuthState()
@@ -50,6 +57,7 @@ export function Sidebar1Adsense() {
 					></ins>
 				</div>
 				<Script
+					id="adsense-script"
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7702053427535735"
 					crossOrigin="anonymous"
 				/>
