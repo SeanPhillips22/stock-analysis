e44d70771139cf876115872b9f3878af64294167
Kris Gunnars <kriistjanm@gmail.com>
2022-01-14 14:25:10 +0000

    Stock Table: Don't show main column in column dropdown

--- components/StockTable/Controls/TableColumns.tsx
+++ components/StockTable/Controls/TableColumns.tsx
@@ -2,8 +2,12 @@ import { SelectColumns } from 'components/Dropdown/SelectColumns/_SelectColumns'
 import { stockTableState } from '../stockTableState'
 
 export function TableColumns() {
+	const main = stockTableState(state => state.main)
 	const columns = stockTableState(state => state.columns)
 	const columnOptions = stockTableState(state => state.columnOptions)
 
-	return <SelectColumns active={columns} options={columnOptions} />
+	let cols = columns.filter(c => c !== main)
+	let colOptions = columnOptions.filter(c => c !== main)
+
+	return <SelectColumns active={cols} options={colOptions} />
 }
--- components/Tables/HeaderCell.tsx
+++ components/Tables/HeaderCell.tsx
@@ -2,20 +2,11 @@ import { DataPoints } from 'data/StockDataPoints'
 import { DataId } from 'types/Data'
 import { ColumnSort } from './ColumnSort'
 
-type Props = {
-	column: {
-		Header: string
-		id: DataId
-		getSortByToggleProps: any
-		render: any
-	}
-}
-
 /**
  * Render a Header cell element with sort functionality
  */
-export function HeaderCell({ column }: Props) {
-	let { Header, id } = column
+export function HeaderCell({ column }: { column: any }) {
+	let { Header, id }: { Header: any; id: DataId } = column
 	let { format } = DataPoints[id]
 
 	let css =
