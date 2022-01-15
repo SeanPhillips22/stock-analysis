e0aee5ccc017676f12777f6f2de7e7dab3dcbe80
Kris Gunnars <kriistjanm@gmail.com>
2022-01-15 06:37:06 +0000

    Set AdSense check interval back to 300ms

--- components/Ads/AdSense/useLoadAdsense.ts
+++ components/Ads/AdSense/useLoadAdsense.ts
@@ -38,10 +38,10 @@ export function useLoadAdsense() {
 				clearInterval(interval)
 			}
 
-			// stop after 20 attempts (4 seconds)
+			// stop after 20 attempts (6 seconds)
 			// usually this works on the first attempt
 			if (count === 20) clearInterval(interval)
-		}, 200)
+		}, 300)
 
 		return () => clearInterval(interval)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
