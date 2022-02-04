dd346dfba4d3997c6a869a394169b9c55e4ffe50
Kris Gunnars <kriistjanm@gmail.com>
2022-02-04 13:04:50 +0000

    Revert test

--- components/News/NewsAds.tsx
+++ components/News/NewsAds.tsx
@@ -1,53 +1,32 @@
-import NewsAd1 from 'components/Ads/Dianomi/NewsAd1'
-import NewsAd2 from 'components/Ads/Dianomi/NewsAd2'
+import dynamic from 'next/dynamic'
+
+const NewsAd1 = dynamic(() => import('components/Ads/Dianomi/NewsAd1'), {
+	ssr: false
+})
+const NewsAd2 = dynamic(() => import('components/Ads/Dianomi/NewsAd2'), {
+	ssr: false
+})
+
+import { LazyLoadAd } from 'components/LazyLoad/_LazyLoadAd'
 
 export function NewsAds({ ad }: { ad: number }) {
 	if (ad === 1) {
 		return (
-			<div className="news-spns" id="ad-banner">
-				<NewsAd1 />
-			</div>
+			<LazyLoadAd offset={300}>
+				<div className="news-spns" id="ad-banner">
+					<NewsAd1 />
+				</div>
+			</LazyLoadAd>
 		)
 	} else if (ad === 2) {
 		return (
-			<div className="news-spns" id="ad-banner">
-				<NewsAd2 />
-			</div>
+			<LazyLoadAd offset={400}>
+				<div className="news-spns" id="ad-banner">
+					<NewsAd2 />
+				</div>
+			</LazyLoadAd>
 		)
 	}
 
 	return null
 }
-
-// import dynamic from 'next/dynamic'
-
-// const NewsAd1 = dynamic(() => import('components/Ads/Dianomi/NewsAd1'), {
-// 	ssr: false
-// })
-// const NewsAd2 = dynamic(() => import('components/Ads/Dianomi/NewsAd2'), {
-// 	ssr: false
-// })
-
-// import { LazyLoadAd } from 'components/LazyLoad/_LazyLoadAd'
-
-// export function NewsAds({ ad }: { ad: number }) {
-// 	if (ad === 1) {
-// 		return (
-// 			<LazyLoadAd offset={300}>
-// 				<div className="news-spns" id="ad-banner">
-// 					<NewsAd1 />
-// 				</div>
-// 			</LazyLoadAd>
-// 		)
-// 	} else if (ad === 2) {
-// 		return (
-// 			<LazyLoadAd offset={400}>
-// 				<div className="news-spns" id="ad-banner">
-// 					<NewsAd2 />
-// 				</div>
-// 			</LazyLoadAd>
-// 		)
-// 	}
-
-// 	return null
-// }
