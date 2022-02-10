a4b1e65ac85e6f680d435040b07b3370bcbb60f7
Kris Gunnars <kriistjanm@gmail.com>
2022-02-10 17:06:33 +0000

    Add mobile1 ad to one article

--- components/Ads/noAds.ts
+++ components/Ads/noAds.ts
@@ -13,6 +13,5 @@ const NO_ADS = [
 
 export function noAds(path: PathType) {
 	if (process.env.NODE_ENV === 'development') return true
-	if (path.one === 'stocks' && path.two === 't') return true // exclude ads on T, for benchmarking purposes
 	return path.one ? NO_ADS.includes(path.one) : false
 }
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -7,10 +7,12 @@ import matter from 'gray-matter'
 import { ArticleLayout } from 'components/Layout/ArticleLayout'
 import { SEO } from 'components/SEO'
 import { CustomLink, External } from 'components/CustomLink'
+import { Mobile1 } from 'components/Ads/AdSense/Mobile1'
 
 const components = {
 	a: CustomLink,
-	External
+	External,
+	Mobile1
 }
 
 interface Props {
