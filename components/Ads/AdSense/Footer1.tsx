3c094c647b85c21368aacbedffdc32eda85c4cd4
Kris Gunnars <kriistjanm@gmail.com>
2022-02-11 09:22:37 +0000

    Footer Ad: Split test Adsense 970x250 against Dianomi

---
+++ components/Ads/AdSense/Footer1.tsx
@@ -0,0 +1,31 @@
+import { authState } from 'state/authState'
+import { useLayoutContext } from 'components/Layout/LayoutContext'
+import { noAds, isDev } from '../noAds'
+import { AdsenseScript } from './AdsenseScript'
+import { useLoadAdsense } from './useLoadAdsense'
+
+export default function Footer1() {
+	const { path } = useLayoutContext()
+	const isPro = authState(state => state.isPro)
+	useLoadAdsense()
+
+	if (noAds(path) || isPro) {
+		return null
+	}
+
+	if (isDev()) {
+		return <div className="adphb ftad"></div>
+	}
+
+	return (
+		<>
+			<ins
+				className="adsbygoogle ftad"
+				data-ad-client="ca-pub-7702053427535735"
+				data-ad-slot="4737940334"
+				data-full-width-responsive="false"
+			></ins>
+			<AdsenseScript />
+		</>
+	)
+}
