f7cb69397b953c2c504b8eea0406b2d1f8b2b810
azidn1nja <azidspider@hotmail.com>
2022-02-15 17:35:55 +0000

    Data is now working. Added various settings, removed zoom and make all points visible without scrolling

--- components/Overview/OverviewStockChart.tsx
+++ components/Overview/OverviewStockChart.tsx
@@ -12,24 +12,47 @@ type Props = {
 
 export default function Chart({ data, time, symbol, close, change }: Props) {
 	const ref = useRef() as React.MutableRefObject<HTMLDivElement>
-	
+
 	useEffect(() => {
 		console.log(data)
 
-		const chart = createChart(ref.current, { width: 650, height: 300 })
-		const lineSeries = chart.addLineSeries()
-		lineSeries.setData([
-			{ time: '2019-04-11', value: 80.01 },
-			{ time: '2019-04-12', value: 96.63 },
-			{ time: '2019-04-13', value: 76.64 },
-			{ time: '2019-04-14', value: 81.89 },
-			{ time: '2019-04-15', value: 74.43 },
-			{ time: '2019-04-16', value: 80.01 },
-			{ time: '2019-04-17', value: 96.63 },
-			{ time: '2019-04-18', value: 76.64 },
-			{ time: '2019-04-19', value: 81.89 },
-			{ time: '2019-04-20', value: 74.43 }
-		])
+		const chart = createChart(ref.current, {
+			width: 650,
+			height: 300,
+			layout: {
+				fontFamily:
+					"system-ui, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'"
+			},
+			rightPriceScale: {
+				borderColor: 'rgba(197, 203, 206, 1)'
+			},
+			crosshair: {
+				mode: 0
+			},
+			timeScale: {
+				borderColor: 'rgba(197, 203, 206, 1)',
+				timeVisible: true
+			},
+			handleScroll: false,
+			handleScale: false
+		})
+
+		const areaSeries = chart.addAreaSeries({
+			topColor: 'rgba(33, 150, 243, 0.56)',
+			bottomColor: 'rgba(33, 150, 243, 0.04)',
+			lineColor: 'rgba(33, 150, 243, 1)',
+			lineWidth: 2
+		})
+
+		const format = data.map(item => {
+			return {
+				time: Math.round(new Date(item.t).getTime() / 1000),
+				value: item.c
+			}
+		})
+
+		areaSeries.setData(format)
+		chart.timeScale().fitContent()
 		return () => {
 			chart.remove()
 		}
