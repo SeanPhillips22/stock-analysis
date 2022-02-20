3dff30239bf5462e4df7c40ef884acf8b8d72129
Kris <kriistjanm@gmail.com>
2022-02-21 05:11:35 +0700

    Test not loading Adsense if it's hidden via CSS on that screen size

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * Can be used to decide whether or not to load an Adsense ad based on the screen size
+ * @param {string} query should be in the format: (min-width: 1024px) -- this hides the ad on screens 1024px and larger
+ * @returns {boolean} true or false
+ */
+function useMediaQuery(query: string): boolean {
+	const getMatches = (query: string): boolean => {
+		// Prevents SSR issues
+		if (typeof window !== 'undefined') {
+			return window.matchMedia(query).matches
+		}
+		return false
+	}
+
+	const [matches, setMatches] = useState<boolean>(getMatches(query))
+
+	function handleChange() {
+		setMatches(getMatches(query))
+	}
+
+	useEffect(() => {
+		const matchMedia = window.matchMedia(query)
+
+		// Triggered at the first client-side load and if query changes
+		handleChange()
+
+		// Listen matchMedia
+		matchMedia.addEventListener('change', handleChange)
+
+		return () => {
+			matchMedia.removeEventListener('change', handleChange)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [query])
+
+	return matches
+}
+
+export default useMediaQuery
