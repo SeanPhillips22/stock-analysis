e2e17f8c560ebaf59720b31ffdfd9b5dae1efc1b
Kris <kriistjanm@gmail.com>
2022-03-07 17:09:53 +0700

    Fix split test

--- components/Ads/AdSense/Sidebar1All.tsx
+++ components/Ads/AdSense/Sidebar1All.tsx
@@ -33,6 +33,7 @@ const A = () => (
 			className="adsbygoogle sb-1a"
 			data-ad-client="ca-pub-7702053427535735"
 			data-ad-slot="4031506518"
+			data-full-width-responsive="true"
 		></ins>
 		<AdsenseScript />
 	</>
