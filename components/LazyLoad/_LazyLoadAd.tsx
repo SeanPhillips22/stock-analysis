739f546f181ca5c0767ed4667284d003007ac2a4
Kris <kriistjanm@gmail.com>
2022-03-10 19:27:37 +0700

    If intersection observer is not defined, load ad without lazy loading

--- components/LazyLoad/_LazyLoadAd.tsx
+++ components/LazyLoad/_LazyLoadAd.tsx
@@ -15,6 +15,18 @@ export function LazyLoadAd({ children, offset, className }: Props) {
 	// Check the nav state
 	const { path } = useLayoutContext()
 
+	// If intersection observer is not defined, then just show the ad
+	// without lazy loading
+	if (typeof window !== 'undefined' && !('IntersectionObserver' in window)) {
+		return (
+			<>
+				{checked && !isPro && !noAds(path) && (
+					<div className={className}>{children}</div>
+				)}
+			</>
+		)
+	}
+
 	// Only load the observer if: a) not pro user and b) not on an excluded page
 	return (
 		<>
