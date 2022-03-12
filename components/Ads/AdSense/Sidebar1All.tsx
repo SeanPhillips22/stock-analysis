c1920e7328367b763125597d853340fdd7d64310
Kris <kriistjanm@gmail.com>
2022-03-12 09:42:09 +0700

    Stop AdSense split test

--- components/Ads/AdSense/Sidebar1All.tsx
+++ components/Ads/AdSense/Sidebar1All.tsx
@@ -10,48 +10,6 @@ import { useLoadAdsense } from './useLoadAdsense'
  * tablet: leaderboard
  * desktop: rectangle
  */
-
-// Alts: Sidebar 1B -- 4031506518
-// Alts: Sidebar 1C -- 1405343174
-
-const Control = () => (
-	<>
-		<ins
-			className="adsbygoogle sb-1a"
-			data-ad-client="ca-pub-7702053427535735"
-			data-ad-slot="8582549443"
-			data-full-width-responsive="false"
-		></ins>
-		<AdsenseScript />
-	</>
-)
-
-// Test version: Sidebar 1B -- Full-width on mobile
-const A = () => (
-	<>
-		<ins
-			className="adsbygoogle sb-1a"
-			data-ad-client="ca-pub-7702053427535735"
-			data-ad-slot="4031506518"
-			data-full-width-responsive="true"
-		></ins>
-		<AdsenseScript />
-	</>
-)
-
-// Control version: Sidebar 1C -- Not full width on mobile
-const B = () => (
-	<>
-		<ins
-			className="adsbygoogle sb-1a"
-			data-ad-client="ca-pub-7702053427535735"
-			data-ad-slot="1405343174"
-			data-full-width-responsive="false"
-		></ins>
-		<AdsenseScript />
-	</>
-)
-
 export function Sidebar1All() {
 	const { path } = useLayoutContext()
 	const isPro = authState(state => state.isPro)
@@ -65,32 +23,15 @@ export function Sidebar1All() {
 		return <div className="adph sb-1a"></div>
 	}
 
-	const ALL_GROUPS = ['A', 'B']
-	type Group = typeof ALL_GROUPS[number]
-	const randomGroup = (): Group =>
-		ALL_GROUPS[Math.floor(Math.random() * ALL_GROUPS.length)]
-
-	const group = randomGroup()
-
-	switch (group) {
-		case 'A':
-			return <A />
-		case 'B':
-			return <B />
-		default:
-			return <Control />
-	}
-
-	/* DEFAULT, BEfORE SPLIT TEST */
-	// return (
-	// 	<>
-	// 		<ins
-	// 			className="adsbygoogle sb-1a"
-	// 			data-ad-client="ca-pub-7702053427535735"
-	// 			data-ad-slot="8582549443"
-	// 			data-full-width-responsive="false"
-	// 		></ins>
-	// 		<AdsenseScript />
-	// 	</>
-	// )
+	return (
+		<>
+			<ins
+				className="adsbygoogle sb-1a"
+				data-ad-client="ca-pub-7702053427535735"
+				data-ad-slot="8582549443"
+				data-full-width-responsive="false"
+			></ins>
+			<AdsenseScript />
+		</>
+	)
 }
