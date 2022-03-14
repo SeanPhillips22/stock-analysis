5b8e370d8282f37c9666a4c5113073139b381878
Kris <kriistjanm@gmail.com>
2022-03-14 14:37:04 +0700

    Update message when no financial forecast available

--- components/Forecasts/Estimates/_Estimates.tsx
+++ components/Forecasts/Estimates/_Estimates.tsx
@@ -1,10 +1,26 @@
+import { useSymbolContext } from 'components/Layout/SymbolContext'
+import { Unavailable } from 'components/Unavailable'
 import { useState } from 'react'
+import { ForecastData } from 'types/Forecast'
 import { EstimatesStats } from './EstimatesStats'
 import { EstimatesTable } from './EstimatesTable'
 import { EstimatesTableControls } from './EstimatesTableControls'
 
 export function Estimates() {
+	const { data }: { data: ForecastData } = useSymbolContext()
 	const [range, setRange] = useState<'Annual' | 'Quarterly'>('Annual')
+
+	if (!data.estimates.table.annual.dates) {
+		return (
+			<div>
+				<h2 className="mb-3 text-xl font-bold">Financial Forecast</h2>
+				<Unavailable
+					message="No financial forecasts available for this stock"
+					classes="min-h-[250px]"
+				/>
+			</div>
+		)
+	}
 
 	return (
 		<div>
