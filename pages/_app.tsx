a6012f04ea2f755bc08acc6daec22d0048a1089b
Kris <kriistjanm@gmail.com>
2022-04-05 10:35:15 +0700

    Install the Plausible Analytics script, default settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,18 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { GoogleTagManager } from 'components/App/GoogleTagManager'
 import { ProgressBar } from 'components/App/ProgressBar'
+import PlausibleProvider from 'next-plausible'
 
 const queryClient = new QueryClient()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<PlausibleProvider domain="stockanalysis.com">
 			<GoogleTagManager />
 			<ProgressBar />
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
 			</QueryClientProvider>
-		</>
+		</PlausibleProvider>
 	)
 }
