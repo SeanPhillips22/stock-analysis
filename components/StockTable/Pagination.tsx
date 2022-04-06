29a71d27d4b21acfba1e6b6bd94ab2b58dd30c4d
Kris <kriistjanm@gmail.com>
2022-04-06 15:50:44 +0700

    Stock Table Pagination: Fix content wider than screen problem

--- components/StockTable/Pagination.tsx
+++ components/StockTable/Pagination.tsx
@@ -21,18 +21,18 @@ export function Pagination({ resultsCount }: { resultsCount?: number }) {
 	}
 
 	return (
-		<nav className="mt-2.5 flex flex-row justify-between space-x-2 px-1 text-sm sm:text-base">
+		<nav className="mt-2.5 flex flex-row justify-between space-x-1 px-1 text-sm bp:space-x-2 sm:text-base">
 			<button
 				onClick={() => previousPage()}
 				disabled={!page[tableId] || page[tableId] === 1}
-				className="relative inline-flex items-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-1 py-1.5 font-medium text-gray-700 hover:bg-gray-50 xs:px-1.5 xs:py-2 sm:pl-1 sm:pr-3"
+				className="relative inline-flex items-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-1 py-1.5 text-xs font-medium text-gray-700 hover:bg-gray-50 xs:px-1.5 xs:py-2 bp:pr-2 bp:text-sm sm:pr-3 sm:pl-1"
 			>
-				<ChevronLeftIcon className="mr-1 -mb-px h-5 w-5 text-gray-600" />
+				<ChevronLeftIcon className="-mb-px h-5 w-5 text-gray-600 bp:mr-1" />
 				Previous
 			</button>
-			<div className="flex flex-row items-center space-x-3 xs:space-x-4">
+			<div className="flex flex-row items-center space-x-2 sm:space-x-4">
 				<span className="whitespace-nowrap">
-					<span className="hidden xs:inline">Page </span>
+					<span className="hidden sm:inline">Page </span>
 					{`${page[tableId] || 1} of ${totalPages}`}
 				</span>
 				<TableResults />
@@ -40,10 +40,10 @@ export function Pagination({ resultsCount }: { resultsCount?: number }) {
 			<button
 				onClick={() => nextPage()}
 				disabled={page[tableId] === totalPages}
-				className="relative inline-flex items-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-1 py-1.5 font-medium text-gray-700 hover:bg-gray-50 xs:px-1.5 xs:py-2 sm:pr-1 sm:pl-3"
+				className="relative inline-flex items-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-1 py-1.5 text-xs font-medium text-gray-700 hover:bg-gray-50 xs:px-1.5 xs:py-2 bp:pl-2 bp:text-sm sm:pl-3 sm:pr-1"
 			>
 				Next
-				<ChevronRightIcon className="ml-1 -mb-px h-5 w-5 text-gray-600" />
+				<ChevronRightIcon className="-mb-px h-5 w-5 text-gray-600 bp:ml-1" />
 			</button>
 		</nav>
 	)
