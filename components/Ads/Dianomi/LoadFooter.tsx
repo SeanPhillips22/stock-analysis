ee6547367fa3b4bb603020b358d1ededca030ac0
kristjanmar <kriistjanm@gmail.com>
2022-04-11 14:32:19 +0700

    Put dianomi footer ad back

--- components/Ads/Dianomi/LoadFooter.tsx
+++ components/Ads/Dianomi/LoadFooter.tsx
@@ -1,25 +1,10 @@
 import dynamic from 'next/dynamic'
-// const FooterAd = dynamic(() => import('components/Ads/Dianomi/FooterAd'), {
-// 	ssr: false
-// })
-const FooterAd = dynamic(() => import('components/Ads/AdSense/FooterAd'), {
-	ssr: false
-})
-const FooterAdBanner = dynamic(() => import('components/Ads/AdSense/FooterAdBanner'), {
+const FooterAd = dynamic(() => import('components/Ads/Dianomi/FooterAd'), {
 	ssr: false
 })
 import { useLayoutContext } from 'components/Layout/LayoutContext'
 
 export function LoadFooter() {
 	const { url } = useLayoutContext()
-
-	const rand = Math.random()
-
-	if (rand < 0.5) {
-		return <FooterAd key={url} />
-	} else {
-		return <FooterAdBanner key={url} />
-	}
-
-	// return <FooterAd key={url} />
+	return <FooterAd key={url} />
 }
