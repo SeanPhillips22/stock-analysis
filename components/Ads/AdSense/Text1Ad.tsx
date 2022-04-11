3f74e7a292fdcec4de0f4c283b5bf53086bc5db9
kristjanmar <kriistjanm@gmail.com>
2022-04-11 17:47:20 +0700

    Text ad

--- components/Ads/AdSense/Text1Ad.tsx
+++ components/Ads/AdSense/Text1Ad.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { authState } from 'state/authState'
 import { useLayoutContext } from 'components/Layout/LayoutContext'
 import { noAds, isDev } from '../noAds'
 import { AdsenseScript } from './AdsenseScript'
 import { useLoadAdsense } from './useLoadAdsense'
-// import { cn } from 'functions/helpers/classNames'
+import { cn } from 'functions/helpers/classNames'
 
 type Props = {
 	mobileOnly?: boolean
@@ -21,19 +20,20 @@ export function Text1Ad({ mobileOnly }: Props) {
 
 	if (isDev()) {
 		return (
-			<div className="ad-banner m1aw">
-				<div className="adph m-1"></div>
+			<div className={cn('ad-banner text1-wrap', mobileOnly ? 'mobile-tablet-only' : '')}>
+				<div className="adph text1"></div>
 			</div>
 		)
 	}
 
 	return (
 		<>
-			<div className="ad-banner m1aw">
+			<div className={cn('ad-banner text1-wrap', mobileOnly ? 'mobile-tablet-only' : '')}>
 				<ins
-					className="adsbygoogle m-1"
+					className="adsbygoogle text1"
 					data-ad-client="ca-pub-7702053427535735"
 					data-ad-slot="3871188988"
+					data-ad-format={mobileOnly ? undefined : 'auto'}
 					data-full-width-responsive="true"
 				></ins>
 			</div>
