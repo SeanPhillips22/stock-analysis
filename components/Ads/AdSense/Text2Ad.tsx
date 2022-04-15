765f4946b45c4adc701f4792833855916e97d60d
Kris <kriistjanm@gmail.com>
2022-04-15 20:08:13 +0700

    Ad test

--- components/Ads/AdSense/Text2Ad.tsx
+++ components/Ads/AdSense/Text2Ad.tsx
@@ -28,7 +28,7 @@ export function Text2Ad() {
 					className="adsbygoogle text2"
 					data-ad-client="ca-pub-7702053427535735"
 					data-ad-slot="4063766260"
-					data-ad-format="auto"
+					data-ad-format="horizontal"
 					data-full-width-responsive="true"
 				></ins>
 			</div>
